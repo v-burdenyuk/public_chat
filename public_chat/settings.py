0400150a8ffcaa9a569b31eef9af15d080b2c3bf
v-burdenyuk <vp.burdenyuk@gmail.com>
2021-04-16 19:23:03 +0300

    adding heroku app address to ALLOWED_HOSTS

--- public_chat/settings.py
+++ public_chat/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cryptic-thicket-03385.herokuapp.com',]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
